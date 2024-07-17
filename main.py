94bc9412bb5d78a801e827ea172639e9b23ddf04
KauanSoaress <gbskauan@gmail.com>
2024-07-17 11:33:15 -0700

    Rotas de deleção e update implementadas

--- main.py
+++ main.py
@@ -41,16 +41,66 @@ def get_game(name):
 def post_game():
     data = open('bd.json')
     games = json.load(data)
+    data.close()
 
     new_game = request.get_json()
-
     games.append(new_game)
 
-    json.dump(games, data)
+    data = open('bd.json', 'w')
+    json.dump(games, data, indent=4)
     data.close()
+
     return make_response(
         jsonify(
             message= 'Game created', 
-            game= jsonify(new_game)
+            game= new_game
+        )
+    )
+
+@app.route('/games/<int:id>', methods=['DELETE'])
+def delete_game(id):
+    data = open('bd.json')
+    games = json.load(data)
+    data.close()
+
+    for i in range(len(games)):
+        if games[i]["id"] == id:
+            games.pop(i)
+            break
+
+    data = open('bd.json', 'w')
+    json.dump(games, data, indent=4)
+    data.close()
+
+    return make_response(
+        jsonify(
+            message= "Game deleted",
+            game= games
+        )
+    )
+
+@app.route('/games/<int:id>', methods=['PUT'])
+def edit_game(id):
+    data = open('bd.json')
+    games = json.load(data)
+    data.close()
+
+    new_game_data = request.get_json()
+
+    for i in range(len(games)):
+        if games[i]["id"] == id:
+            games[i]["id"] = new_game_data["id"]
+            games[i]["name"] = new_game_data["name"]
+            games[i]["year"] = new_game_data["year"]
+            break
+    
+    data = open('bd.json', 'w')
+    json.dump(games, data, indent=4)
+    data.close()
+
+    return make_response(
+        jsonify(
+            message= "Game edited",
+            game= new_game_data
         )
     )
