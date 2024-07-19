057f431501f9f570a6b7056ac81398964c9cef39
KauanSoaress <gbskauan@gmail.com>
2024-07-19 14:09:12 -0300

    Verificações de entradas e retornos diferentes implementados

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 - Agrupar funções
 - Separar a chamada das rotas e definição das funções para outra função
 - Diminuir a quantidade de vezes que o JSON é aberto
+- Adicionar tratamentos para verificar se o dado existe, realizar um tratamento da entrada de dados (Passar os dados para um formulário para realizar verificações
 
 # TO-DO
-
-- Adicionar tratamentos para verificar se o dado existe, realizar um tratamento da entrada de dados (Passar os dados para um formulário para realizar verificações
--- blueprints/games/games_defs.py
+++ blueprints/games/games_defs.py
@@ -1,4 +1,5 @@
 from flask import jsonify, request, make_response
+from .VerifyParameters import verify_name_and_year, exists_id
 import json
 
 def get_or_post_games():
@@ -7,6 +8,14 @@ def get_or_post_games():
         games = json.load(data)
         data.close()
         
+        if len(games) == 0:
+            return make_response(
+                jsonify(
+                    message= 'Games list is empty',
+                ),
+                204
+            )
+
         return make_response(
             jsonify(
                 message= 'Games list', 
@@ -18,6 +27,14 @@ def get_or_post_games():
             games = json.load(data)
 
             new_game = request.get_json()
+
+            if not verify_name_and_year(new_game):
+                return make_response(
+                    jsonify(
+                        message= 'Register Error: Invalid name or year',
+                    ),
+                    422
+                )
         
             position_to_insert = str(len(games) + 1)
 
@@ -52,11 +69,25 @@ def get_game_by_name(name):
                     }
                 )
             )
+        
+    return make_response (
+        jsonify (
+            message= "Game not found"
+        )
+    )
 
 def delete_or_edit_game(id):
     if request.method == 'DELETE':
         with open('bd.json', 'r+') as data:
             games = json.load(data)
+
+            if not exists_id(id, games):
+                return make_response(
+                    jsonify(
+                        message= 'Register Error: Invalid id',
+                    ),
+                    422
+                )
 
             games.pop(id)
 
@@ -77,6 +108,22 @@ def delete_or_edit_game(id):
             games = json.load(data)
 
             new_game_data = request.get_json()
+
+            if not exists_id(id, games):
+                return make_response(
+                    jsonify(
+                        message= 'Register Error: Invalid id',
+                    ),
+                    422
+                )
+
+            if not verify_name_and_year(new_game_data):
+                return make_response(
+                    jsonify(
+                        message= 'Register Error: Invalid name or year',
+                    ),
+                    422
+                )
 
             games[id]["name"] = new_game_data["name"]
             games[id]["year"] = new_game_data["year"]
