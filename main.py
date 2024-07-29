73d01c87a0f2b5a83571e13c667cbef62cd8407d
KauanSoaress <gbskauan@gmail.com>
2024-07-29 15:57:06 -0300

    exclusão do raw.py e alteração de comentário na main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 app.json.sort_keys = False
 
-# Set up MongoDB client
+# Conexão com o Mongo
 mongo_client = MongoClient('mongodb://localhost:27017/')
 app.config['MONGO_CLIENT'] = mongo_client
 app.config['MONGO_DB'] = mongo_client['gamesData']
