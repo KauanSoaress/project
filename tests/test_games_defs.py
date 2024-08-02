5dc40d6ec71fe77fbeb58505d857409081bbee68
KauanSoaress <gbskauan@gmail.com>
2024-08-02 16:07:05 -0300

    Testes implementados

---
+++ tests/test_games_defs.py
@@ -0,0 +1,97 @@
+import unittest
+from unittest.mock import patch, Mock, MagicMock
+from bson import ObjectId
+from flask import Flask
+from utils.games_defs import get_games, post_games, get_game_by_name, delete_game, edit_game
+
+class TestGamesDefs(unittest.TestCase):
+
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['MONGO_DB'] = MagicMock()
+        self.mock_games_collection = Mock()
+        self.app.config['MONGO_DB'].__getitem__.return_value = self.mock_games_collection
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.request_context = self.app.test_request_context()
+        self.request_context.push()
+
+
+    def test_get_games_success(self):
+        mock_find = self.mock_games_collection.find
+        mock_find.return_value = [
+            {"name": "Game 1", "year": 2021},
+            {"name": "Game 2", "year": 2022}
+        ]
+
+        response = get_games()
+
+        mock_find.assert_called_once_with({}, {"_id": 0, "name": 1, "year": 1})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Games list', response.get_json()['message'])
+        self.assertEqual(len(response.get_json()['games']), 2)
+        self.assertEqual(response.get_json()['games'][0]['name'], 'Game 1')
+        self.assertEqual(response.get_json()['games'][1]['name'], 'Game 2')
+
+    def test_get_game_by_name_sucess(self):
+        mock_find_one = self.mock_games_collection.find_one
+        mock_find_one.return_value = {"name": "Game", "year": 2021}
+
+        response = get_game_by_name('Game')
+
+        mock_find_one.assert_called_once_with({'name': 'Game'}, {"_id": 0, "name": 1, "year": 1})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Game found', response.get_json()['message'])
+
+    @patch('utils.games_defs.verify_name')
+    @patch('utils.games_defs.verify_year')
+    def test_post_games(self, mock_verify_year, mock_verify_name):
+        mock_insert = self.mock_games_collection.insert_one
+        mock_insert.return_value = Mock(inserted_id=ObjectId('66a3d3e92646ece2a8c4e49c'))
+
+        mock_verify_year.return_value = True
+        mock_verify_name.return_value = True
+    
+        with self.app.test_request_context(json={"name": "Game 1", "year": 2021}):
+            response = post_games()
+
+            mock_insert.assert_called_once_with({"name": "Game 1", "year": 2021, '_id': '66a3d3e92646ece2a8c4e49c'})
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Game created', response.get_json()['message'])
+
+    @patch('utils.games_defs.verify_id')
+    def test_delete_game_success(self, mock_verify_id):
+        mock_delete_one = self.mock_games_collection.delete_one
+
+        mock_verify_id.return_value = True
+
+        response = delete_game('66a3d3e92646ece2a8c4e49c')
+
+        mock_delete_one.assert_called_once_with({'_id': ObjectId('66a3d3e92646ece2a8c4e49c')})
+
+        self.assertEqual(response.status_code, 200)
+
+    @patch('utils.games_defs.verify_name')
+    @patch('utils.games_defs.verify_year')
+    @patch('utils.games_defs.verify_id')
+    def test_put_games(self, mock_verify_id, mock_verify_year, mock_verify_name):
+        mock_update = self.mock_games_collection.update_one
+
+        mock_verify_year.return_value = True
+        mock_verify_name.return_value = True
+        mock_verify_id.return_value = True
+    
+        with self.app.test_request_context(json={"name": "Game 1", "year": 2021}):
+            response = edit_game('66a3d3e92646ece2a8c4e49c')
+
+            mock_update.assert_called_once_with({'_id': ObjectId('66a3d3e92646ece2a8c4e49c')}, {'$set': {'name': 'Game 1', 'year': 2021}})
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Game edited', response.get_json()['message'])
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_verifyParameters.py
@@ -0,0 +1,127 @@
+from utils.VerifyParameters import verify_name, verify_year, verify_id
+import unittest
+
+class TestsVerifyName(unittest.TestCase):
+
+    def test_verify_name(self):
+        data = {
+            "name": "The Last of Us",
+        }
+
+        correct_data = verify_name(data)    
+
+        self.assertTrue(correct_data, "Falha com dados corretos")
+        
+    def test_verify_name_field_missing(self):
+        data = {
+            "year": 123456,
+        }
+
+        without_name = verify_name(data)
+
+        self.assertFalse(without_name, "Falha com campo faltando")
+
+    def test_verify_name_type_number(self):
+        data = {
+            "name": 123456,
+        }
+
+        name_type_number = verify_name(data)
+
+        self.assertFalse(name_type_number, "Falha com tipo trocado (number)")
+
+    def test_verify_name_type_boolean(self):
+        data = {
+            "name": True,
+        }
+
+        name_type_boolean = verify_name(data)
+
+        self.assertFalse(name_type_boolean, "Falha com tipo trocado (boolean)")
+
+class TestsVerifyYear(unittest.TestCase):
+    def test_verify_year(self):
+        data = {
+            "year": 2004,
+        }
+
+        correct_data = verify_year(data)    
+
+        self.assertTrue(correct_data, "Falha com dados corretos")
+        
+    def test_verify_year_field_missing(self):
+        data = {
+            "name": 123456,
+        }
+
+        without_year = verify_year(data)
+
+        self.assertFalse(without_year, "Falha com campo faltando")
+
+    def test_verify_year_type_string(self):
+        data = {
+            "year": "hoje",
+        }
+
+        year_type_string = verify_year(data)
+
+        self.assertFalse(year_type_string, "Falha com tipo trocado (string)")
+
+    def test_verify_year_type_boolean(self):
+        data = {
+            "year": True,
+        }
+
+        year_type_boolean = verify_year(data)
+
+        self.assertFalse(year_type_boolean, "Falha com tipo trocado (boolean)")
+
+    def test_verify_year_small_field_lenght(self):
+        data = {
+            "year": 201,
+        }
+
+        year_small_field = verify_year(data)
+
+        self.assertFalse(year_small_field, "Erro de tamanho do campo year (small)")
+
+    def test_verify_year_big_field_lenght(self):
+        data = {
+            "year": 20112,
+        }
+
+        year_big_field = verify_year(data)
+
+        self.assertFalse(year_big_field, "Erro de tamanho do campo year (big)")
+
+class TestsVerifyId(unittest.TestCase):
+    def test_verify_id(self):
+        id = "5f7d2a9f1c9d440000f0b7d5"
+
+        correct_id = verify_id(id)
+
+        self.assertTrue(correct_id, "Falha com id correto")
+    
+    def test_verify_id_wrong_id(self):
+        id = "5f7d2a9f1c9d440000f0b7d"
+
+        wrong_id = verify_id(id)
+
+        self.assertFalse(wrong_id, "Falha com id errado")
+    
+    def test_verify_id_wrong_id_type(self):
+        id = 123456
+
+        wrong_id_type = verify_id(id)
+
+        self.assertFalse(wrong_id_type, "Falha com tipo errado (number)")
+    
+    def test_verify_id_wrong_id_type_boolean(self):
+        id = True
+
+        wrong_id_type_boolean = verify_id(id)
+
+        self.assertFalse(wrong_id_type_boolean, "Falha com tipo errado (boolean)")
+
+if __name__ == '__main__':
+    unittest.main()
